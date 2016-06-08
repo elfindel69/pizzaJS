18e3e234571b166e28cc0fa28746d9ac1cf517ca
Valentin Michel <elfindel69@gmail.com>
2016-06-08 14:30:02 +0200

    manips tableaux

--- formation-dta/src/index.js
+++ formation-dta/src/index.js
@@ -7,3 +7,24 @@ console.log(p)
 p.setName('titi')
 console.log(p)
 
+var isEven = function (v) {
+  return v % 2 === 0
+}
+
+var filtre = function (array, condition) {
+  return array.reduce(function (acc, index) {
+    if (condition(index)) {
+      acc.push(index)
+    }
+    return acc
+  }
+  , [])
+}
+
+var evens1 = [1, 2, 3, 4].filter(isEven)
+var evens2 = filtre([1, 2, 3, 4], isEven)
+var odds2 = filtre([1, 2, 3, 4], function (item) {
+  return item % 2 !== 0
+})
+
+console.log(evens1, evens2, odds2)
