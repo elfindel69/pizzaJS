2abb5b3fdb2a8e4174a2d29f83dd0de316a6f26d
Valentin Michel <elfindel69@gmail.com>
2016-06-10 11:38:01 +0200

    modifs

--- formation-dta/src/index.js
+++ formation-dta/src/index.js
@@ -108,34 +108,3 @@ pizzaList.getPizzas()
     tabPizzas.appendChild(tr)
   }
 ))
-
-// pizzaList.pizzas[0]
-//   .cook(2000)
-//   .then((pizza) => {
-//     console.log('Bing ! Pizza cuite', pizza)
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   })
-
-// setTimeout(function () {
-//   pizzaList.pizzas[0]
-//     .cook(1000)
-//     .then((pizza) => {
-//       console.log('Bing ! Pizza cuite', pizza)
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// }, 1000)
-
-// setTimeout(function () {
-//   pizzaList.pizzas[0]
-//     .cook(1000)
-//     .then(() => {
-//       console.log('Bing ! Pizza cuite')
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// }, 2500)
