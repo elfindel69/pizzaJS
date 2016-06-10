3f8e371cad42961ff267913b03f17d65e5ea47d8
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-10 12:25:48 +0200

    Refactor (suite)
    function createTableRows (pizza)

--- formation-dta/src/index.js
+++ formation-dta/src/index.js
@@ -10,29 +10,6 @@ tabPizzas.style.border = '1px solid black'
 
 createTableHeader()
 
-// header
-// var tabHeader = document.createElement('th')
-
-// // nom
-// var headerName = document.createElement('td')
-// headerName.style.border = '1px solid black'
-// headerName.innerHTML = 'NOM'
-// tabHeader.appendChild(headerName)
-
-// // toppings
-// var headerToppings = document.createElement('td')
-// headerToppings.style.border = '1px solid black'
-// headerToppings.innerHTML = 'TOPPINGS'
-// tabHeader.appendChild(headerToppings)
-
-// // statut
-// var headerStatus = document.createElement('td')
-// headerStatus.style.border = '1px solid black'
-// headerStatus.innerHTML = 'STATUS'
-// tabHeader.appendChild(headerStatus)
-
-// tabPizzas.appendChild(tabHeader)
-
 // liste des pizzas
 pizzaList.getPizzas()
 // map : création des objets pizza
@@ -44,53 +21,7 @@ pizzaList.getPizzas()
 // affichage
 .then(pizzas => pizzas.forEach(
   pizza => {
-    // pizza 
-    var tr = document.createElement('tr')
-
-    // affichage nom
-    var tdName = document.createElement('td')
-    tdName.style.border = '1px solid black'
-    tdName.innerHTML = pizza.name
-    tr.appendChild(tdName)
-
-   // affichage liste toppings
-    var tdToppings = document.createElement('td')
-    tdToppings.style.border = '1px solid black'
-    tdToppings.innerHTML = pizza.toppings2string()
-    tr.appendChild(tdToppings)
-
-    // affichage statut
-    var tdStatus = document.createElement('td')
-    tdStatus.style.border = '1px solid black'
-    if (pizza.status === 0) {
-      tdStatus.innerHTML = 'RAW'
-    } else if (pizza.status === 1) {
-      tdStatus.innerHTML = 'COOKING'
-    } else {
-      tdStatus.innerHTML = 'COOKED'
-    }
-    tr.appendChild(tdStatus)
-
-    // affichage bouton cook
-    var tdCook = document.createElement('td')
-    tdCook.style.border = '1px solid black'
-    var btnCook = document.createElement('button')
-    btnCook.innerHTML = 'cook'
-    btnCook.addEventListener('click', evt => {
-      // cuisson de la pizza
-      pizza.cook(2000)
-        .then((pizza) => {
-          tdStatus.innerHTML = 'COOKED'
-        })
-        .catch(err => {
-          window.alert(err)
-          console.log(err)
-        })
-    })
-    tdCook.appendChild(btnCook)
-    tr.appendChild(tdCook)
-
-    tabPizzas.appendChild(tr)
+    createTableRows(pizza)
   }
 ))
 
@@ -120,8 +51,54 @@ function createTableHeader () {
   tabPizzas.appendChild(tabHeader)
 }
 
-// fonction création cases du tableau
-function createTableRows (params) {
-  // à implémenter
+// fonction création lignes du tableau
+function createTableRows (pizza) {
+  // pizza 
+  var tr = document.createElement('tr')
+
+  // affichage nom
+  var tdName = document.createElement('td')
+  tdName.style.border = '1px solid black'
+  tdName.innerHTML = pizza.name
+  tr.appendChild(tdName)
+
+   // affichage liste toppings
+  var tdToppings = document.createElement('td')
+  tdToppings.style.border = '1px solid black'
+  tdToppings.innerHTML = pizza.toppings2string()
+  tr.appendChild(tdToppings)
+
+  // affichage statut
+  var tdStatus = document.createElement('td')
+  tdStatus.style.border = '1px solid black'
+  if (pizza.status === 0) {
+    tdStatus.innerHTML = 'RAW'
+  } else if (pizza.status === 1) {
+    tdStatus.innerHTML = 'COOKING'
+  } else {
+    tdStatus.innerHTML = 'COOKED'
+  }
+  tr.appendChild(tdStatus)
+
+  // affichage bouton cook
+  var tdCook = document.createElement('td')
+  tdCook.style.border = '1px solid black'
+  var btnCook = document.createElement('button')
+  btnCook.innerHTML = 'cook'
+  btnCook.addEventListener('click', evt => {
+    // cuisson de la pizza
+    pizza.cook(2000)
+        .then((pizza) => {
+          tdStatus.innerHTML = 'COOKED'
+        })
+        .catch(err => {
+          window.alert(err)
+          console.log(err)
+        })
+  })
+  tdCook.appendChild(btnCook)
+  tr.appendChild(tdCook)
+
+  tabPizzas.appendChild(tr)
 }
 
