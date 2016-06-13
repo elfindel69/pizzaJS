b34f83b92a18299c5788ec743de064f273bc4a1e
Valentin Michel <elfindel69@gmail.com>
2016-06-13 12:22:17 +0200

    modifs, création de module et controller

--- dtang/src/index.js
+++ dtang/src/index.js
@@ -1,1 +1,29 @@
 import angular from 'angular'
+
+angular.module('dtang', [])
+
+  .config(function () {
+    console.log('DTANG CONFIG')
+  })
+  .value('AppName', 'DTANG')
+  .run(function (AppName) {
+    console.log(AppName + ' START')
+  })
+
+  .controller('MonController', MonController)
+
+angular.bootstrap(document, ['dtang'])
+
+
+function MonController () {
+  console.log('Mon Controller')
+  const monCtrl = this
+  monCtrl.message = 'Hello '
+
+  monCtrl.user = {
+    name: 'Thomas'
+  }
+  monCtrl.getName = function () {
+    return 'Thomas'
+  }
+}
