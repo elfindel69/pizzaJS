7c584fe6227d5da14d3ee21cec9a0b40fb971e06
Valentin Michel <elfindel69@gmail.com>
2016-06-15 10:47:57 +0200

    service classe

--- dtang/src/pizza/index.js
+++ dtang/src/pizza/index.js
@@ -2,7 +2,8 @@ import angular from 'angular'
 import { PizzaListController } from './pizza-list.controller'
 import { PizzaController } from './pizza.controller'
 import { pizzaToppingsFilter } from './pizza-toppings.filter'
-import { Pizza } from './pizza'
+
+import { PizzaService } from './pizza.service'
 export const PizzaModule =
 
 angular.module('dtang.pizza', [])
@@ -11,20 +12,5 @@ angular.module('dtang.pizza', [])
     .controller('PizzaController', PizzaController)
 
     .filter('pizzaToppings', pizzaToppingsFilter)
-    .factory('PizzaService', function ($timeout) {
-      var pizzas = [
-        new Pizza({ name: 'Pizza 1', status: 0, toppings: ['eggs', 'mushrooms'] }),
-        new Pizza({ name: 'Pizza 2', status: 1, toppings: [] }),
-        new Pizza({ name: 'Pizza 3', status: 0, toppings: ['eggs', 'eggs', 'mushrooms'] }),
-        new Pizza({ name: 'Pizza 4', status: 1 }),
-        new Pizza({ name: 'Pizza 5', status: 0 })
-      ]
-
-      return {
-        name: 'PizzaService',
-        getPizzas () {
-          return $timeout(2000).then(() => pizzas)
-        }
-      }
-    })
+    .service('PizzaService', PizzaService)
     .name
---
+++ dtang/src/pizza/pizza.service.js
@@ -0,0 +1,20 @@
+import { Pizza } from './pizza'
+
+var pizzas = [
+  new Pizza({ name: 'Pizza 1', status: 0, toppings: ['eggs', 'mushrooms'] }),
+  new Pizza({ name: 'Pizza 2', status: 1, toppings: [] }),
+  new Pizza({ name: 'Pizza 3', status: 0, toppings: ['eggs', 'eggs', 'mushrooms'] }),
+  new Pizza({ name: 'Pizza 4', status: 1 }),
+  new Pizza({ name: 'Pizza 5', status: 0 })
+]
+
+export class PizzaService {
+  constructor ($timeout) {
+    this.$timeout = $timeout
+  }
+  getPizzas () {
+    return this.$timeout(2000).then(() => pizzas)
+  }
+}
+
+PizzaService.$inject = ['$timeout']
