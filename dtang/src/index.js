5b32bfe5cf0970bf7de3e67b16b35df0a5da2782
Valentin Michel <elfindel69@gmail.com>
2016-06-16 11:58:49 +0200

    test controlleur

--- dtang/src/index.js
+++ dtang/src/index.js
@@ -2,6 +2,19 @@ import angular from 'angular'
 import ngRoute from 'angular-route'
 import { PizzaModule } from './pizza'
 
+class DtaUserDirectiveController {
+  constructor ($timeout) {
+    console.log('Instanciation du contrôleur')
+    $timeout(() => {
+      this.onAction({
+        $event: {
+          color: this.color
+        }
+      })
+    }, 1000)
+  }
+}
+
 angular.module('dtang', [
   PizzaModule, ngRoute
 ])
@@ -24,5 +37,32 @@ angular.module('dtang', [
 })
 .otherwise('/')
 })
+
+.component('dtaUser', {
+  bindings: {
+    user: '=',
+    color: '@userColor',
+    onAction: '&'
+  },
+  transclude: true,
+  template: `
+    <h2>
+      <strong ng-transclude></strong>
+      {{ $ctrl.user }}
+    </h2>
+  `,
+  controller: DtaUserDirectiveController
+})
+
+
+  .run(function ($rootScope) {
+    $rootScope.user2 = { name: 'Thomas' }
+    // $rootScope.$on('$routeChangeStart', (event, next, current) => {
+    //   console.log('$routeChangeStart', event)
+    // })
+    $rootScope.test = function () {
+      console.log('TEST')
+    }
+  })
 
 angular.bootstrap(document, ['dtang'])
