82bf73d9a23fd120307dd02be09435945ffedb5d
Valentin Michel <elfindel69@gmail.com>
2016-06-16 14:49:39 +0200

    modif pizza component ajout/suppression topping by click

--- dtang/src/pizza/pizza-toppings.component.js
+++ dtang/src/pizza/pizza-toppings.component.js
@@ -1,6 +1,24 @@
+class PizzaToppingsComponentController {
+  $onChanges (changes) {
+    if (changes.allToppings && this.allToppings) {
+      this.allToppings = Object.keys(this.allToppings)
+    }
+  }
+
+  removeTopping (id) {
+    this.toppings.splice(id, 1)
+  }
+
+  addTopping (topping) {
+    this.toppings.push(topping)
+  }
+}
+
 export const PizzaToppingsComponent = {
   bindings: {
-    toppings: '<list'
+    toppings: '<',
+    allToppings: '<'
   },
-  templateUrl: 'pizza-toppings.html'
+  templateUrl: 'pizza-toppings.html',
+  controller: PizzaToppingsComponentController
 }
